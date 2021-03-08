1b607f1c726c5a93a8ccbbe2f66c57cb9b2fc6d2
DustinDiazLopez <hunterghost526@gmail.com>
2021-03-08 02:54:25 -0400

    Initial Commit

--- src/main/java/com/example/marlace/MarlaceApplication.java
+++ src/main/java/com/example/marlace/MarlaceApplication.java
@@ -1,4 +1,4 @@
-package diaz.dustin.marlace;
+package com.example.marlace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/marlace/MarlaceApplicationTests.java
+++ src/test/java/com/example/marlace/MarlaceApplicationTests.java
@@ -1,4 +1,4 @@
-package diaz.dustin.marlace;
+package com.example.marlace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MarlaceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
