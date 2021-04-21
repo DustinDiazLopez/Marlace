1bf6fd3f02958f78b17e92eae9d7f043ef33c371
DustinDiazLopez <hunterghost526@gmail.com>
2021-04-20 21:21:03 -0500

    diabled circleci

--- src/test/java/com/example/marlace/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.marlace;
-
-public final class Constants {
-
-    public static final class Server {
-        public static final String URL = "http://localhost:%d/api";
-        public static final String USER_URL = URL + "/u";
-    }
-}
